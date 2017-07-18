1201cb01d3d91b67075e9250d2fba8e2545efca1
Matt Wagner <m.wagner@dwdiesel.com>
2017-07-18 16:29:14 -0400

    Add additional bindings per the exercise

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,20 +4,55 @@ import { Component } from '@angular/core';
   selector: 'my-app', // to use me, do this in html <my-app>
   template: `
     <h1>{{name}}</h1>
+
+    <p><i>{{name}} is in the {{region}} region.</i></p>
+
+    <br>
+    <button (click)="addressClick()">Show/Hide Address</button>
+
+    <div [hidden]="hideAddress">
+      <h3>Address:</h3>
+      <fieldset>
+        <label>Street: </label>{{street}}
+      </fieldset>
+      <fieldset>
+        <label>City: </label>{{city}}
+      </fieldset>
+      <fieldset>
+        <label>
+          Region:
+          <select (change)="regionChange($event.target.value)">
+            <option>North</option>
+            <option>South</option>
+            <option>East</option>
+            <option>West</option>
+          </select>
+        </label>
+      </fieldset>
+    </div>
+
     <fieldset>
       <img [src]="image"/>
     </fieldset>
-    <label [style.color]="color">Favorite Color</label>
-    <button (click)="clicked()">Toggle Color</button>
-    <select (change)="colorChange($event.target.value)">
-      <option>red</option>
-      <option>blue</option>
-      <option>green</option>
-    </select>
+
+    <fieldset>
+      <label [style.color]="color">Favorite Color</label>
+      <button (click)="clicked()">Toggle Color</button>
+      <select (change)="colorChange($event.target.value)">
+        <option>red</option>
+        <option>blue</option>
+        <option>green</option>
+      </select>
+    </fieldset>
+
   `,
 })
 export class AppComponent {
   name = 'Alex Smith';
+  street = '1503 Clark Street Rd';
+  city = 'Auburn';
+  region = 'East';
+  hideAddress = false;
   image = 'favicon.ico';
   color = 'red';
 
@@ -29,4 +64,11 @@ export class AppComponent {
     this.color = color;
   }
 
+  addressClick() {
+    this.hideAddress = !this.hideAddress;
+  }
+
+  regionChange(region: string) {
+    this.region = region;
+  }
 }
