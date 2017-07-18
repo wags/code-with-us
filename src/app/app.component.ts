72924af776138d834573f4a9d035bf86642205fb
Matt Wagner <m.wagner@dwdiesel.com>
2017-07-18 15:35:14 -0400

    Demonstrate property and event binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,31 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app', // to use me, do this in html <my-app>
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `
+    <h1>{{name}}</h1>
+    <fieldset>
+      <img [src]="image"/>
+    </fieldset>
+    <label [style.color]="color">Favorite Color</label>
+    <button (click)="clicked()">Toggle Color</button>
+    <select (change)="colorChange($event.target.value)">
+      <option>red</option>
+      <option>blue</option>
+      <option>green</option>
+    </select>
+  `,
 })
 export class AppComponent {
-  name = 'Angular';
+  name = 'Alex Smith';
+  image = 'favicon.ico';
+  color = 'red';
+
+  clicked() {
+    this.color = this.color === 'red' ? 'blue' : 'red';
+  }
+
+  colorChange(color: string) {
+    this.color = color;
+  }
+
 }
