d220d648d7c8222b2ee3c19eeb76b94d6c66c6bd
Matt Wagner <m.wagner@dwdiesel.com>
2017-07-18 15:02:58 -0400

    Add comments in key code files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
 
+// This describes the class that follows it
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  imports:      [ BrowserModule ], // what stuff do I need?
+  declarations: [ AppComponent ], // what things are in my app?
+  bootstrap:    [ AppComponent ] // where do I start?
 })
 export class AppModule { }
