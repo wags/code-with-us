833d98bb50b371ae6eeb68213b061b6a6d2e91aa
Matt Wagner <m.wagner@dwdiesel.com>
2017-07-18 16:56:45 -0400

    Demonstrate two-way binding on input fields

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,22 @@ import { Component } from '@angular/core';
   selector: 'my-app', // to use me, do this in html <my-app>
   template: `
     <h1>{{name}}</h1>
-
     <p><i>{{name}} is in the {{region}} region.</i></p>
-
     <br>
-    <button (click)="addressClick()">Show/Hide Address</button>
 
+    <fieldset>
+      <label>Name:</label><br>
+      <input [value]="name"><br>
+      <input [value]="name" (input)="name=$event.target.value"><br>
+      <input [value]="name" (keyup)="name=$event.target.value"><br>
+      <input [value]="name" (keyup.enter)="name=$event.target.value"><br>
+      <input [value]="name" (keyup.enter)="name=$event.target.value"
+                            (blur)="name=$event.target.value"><br>
+      <input [(ngModel)]="name"><br>
+      <input [ngModel]="name" (ngModelChange)="name=$event"><br>
+    </fieldset>
+
+    <button (click)="addressClick()">Show/Hide Address</button>
     <div [hidden]="hideAddress">
       <h3>Address:</h3>
       <fieldset>
