869b7b32b1cee49cca20a490d2ca8ad17402ac48
Matt Wagner <m.wagner@dwdiesel.com>
2017-07-18 16:56:45 -0400

    Demonstrate two-way binding on input fields

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
-import { NgModule }      from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
-import { AppComponent }  from './app.component';
+import { AppComponent } from './app.component';
 
 // This describes the class that follows it
 @NgModule({
-  imports:      [ BrowserModule ], // what stuff do I need?
-  declarations: [ AppComponent ], // what things are in my app?
-  bootstrap:    [ AppComponent ] // where do I start?
+  imports: [BrowserModule, FormsModule], // what stuff do I need?
+  declarations: [AppComponent], // what things are in my app?
+  bootstrap: [AppComponent] // where do I start?
 })
 export class AppModule { }
