09f4fbc1270b2232f71e6f17d4750544c182c7ef
Matt Wagner <m.wagner@dwdiesel.com>
2017-07-21 11:19:29 -0400

    Add buttons to shift left and right in customer detail

--- src/app/customer-detail.component.ts
+++ src/app/customer-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 import { Customer } from './model';
 
@@ -9,6 +9,16 @@ import { Customer } from './model';
 })
 export class CustomerDetailComponent {
   @Input() customer: Customer;
+  @Output() shift = new EventEmitter<number>();
 
   showAddress = true;
+
+  right() {
+    this.shift.emit(1);
+  }
+
+  left() {
+    this.shift.emit(-1);
+  }
+
 }
