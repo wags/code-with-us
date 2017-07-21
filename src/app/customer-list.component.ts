a3c908268e804755f269069e2ef12bc1c5b7f153
Matt Wagner <m.wagner@dwdiesel.com>
2017-07-21 11:25:39 -0400

    Listen for shift events and change customer accordingly

--- src/app/customer-list.component.ts
+++ src/app/customer-list.component.ts
@@ -53,4 +53,10 @@ export class CustomerListComponent {
       }
     },
   ];
+
+  shift(increment: number) {
+    let ix = this.customers.findIndex(c => c === this.customer) + increment;
+    ix = Math.min(this.customers.length - 1, Math.max(0, ix));
+    this.customer = this.customers[ix];
+  }
 }
