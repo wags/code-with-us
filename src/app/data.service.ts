47855a989df3ccc5249b8c43f3c95efc04b6a8dd
Matt Wagner <m.wagner@dwdiesel.com>
2017-07-24 08:24:18 -0400

    Use the logger service in the data service

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,10 +1,15 @@
 import { Injectable } from '@angular/core';
 
 import { createTestCustomers } from './test-data';
+import { LoggerService } from './logger.service';
 
 @Injectable()
 export class DataService {
+  constructor(private loggerService: LoggerService) { }
+
   getCustomers() {
-    return createTestCustomers();
+    const customers = createTestCustomers();
+    this.loggerService.log(`Got ${customers.length} customers`);
+    return customers;
   }
 }
